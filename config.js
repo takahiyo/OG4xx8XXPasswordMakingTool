19fc7e8352917c459b64dbfb2ee46fc12ef8d6b2
takahiyo <141232864+takahiyo@users.noreply.github.com>
2026-01-23 11:30:52 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const AppConfig = {
-    // ↓ "https://..." の部分を、先ほどコピーしたWorkerのURLに書き換えてください
-    API_ENDPOINT: "https://og4xx8xxpasswordmakingtool.taka-hiyo.workers.dev/",
+    API_ENDPOINT: "https://og4xx8xxpasswordmakingtool-dev.taka-hiyo.workers.dev/",
     UI: {
         MAX_WIDTH: "400px",
         COLORS: {
